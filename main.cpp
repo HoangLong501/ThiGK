320905587b9b85f98eecf43c791d8ddc8f154eab
HoangLong501 <longkkbb@gmail.com>
2023-11-13 13:27:38 +0700

    chinhphuong

--- main.cpp
+++ main.cpp
@@ -6,5 +6,19 @@ int main(){
 	int n;
 	cout<<"Nhap vao n so nguyen";
 	cin>>n;
+	if(ktChinhphuong(n)==true){
+			cout<<"chinhphuong";
+	}else cout<<"ko phai cp";
 	return 0;
+	
+	
+}
+
+bool ktChinhphuong(int cp){
+		int s=0;
+		for(int i=0;i<n;i++){
+			if(cp%i==0)
+				s+=i;
+		}
+		return s==n;
 }
